66d6f3ac9cb8f6e55a9cc150f369e9c7d31d98a0
magic1979 <salvatore.bruni@gmail.com>
2016-10-02 00:50:27 +0200

    0k898hhh

--- www/js/index.js
+++ www/js/index.js
@@ -28,43 +28,63 @@ receivedEvent: function(id) {
 	//////// PUSH NUOVE //////
 	
 	
-	var push = PushNotification.init({
-		
-    android: {
-        senderID: "12250132047"
-    },
-    browser: {
-        pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-    },
-    ios: {
-        alert: "true",
-        badge: "true",
-        sound: "true"
-    },
-    windows: {}
-	});
+	var pushNotification;
+	var token
 
-	push.on('registration', function(data) {
-		// data.registrationId
-		var token = data.registrationId
-		alert(token)
-	});
+
 	
-	push.on('notification', function(data) {
-		// data.message,
-		// data.title,
-		// data.count,
-		// data.sound,
-		// data.image,
-		// data.additionalData
-	});
+	pushNotification = window.plugins.pushNotification;
 	
-	push.on('error', function(e) {
-		// e.message
-	});
+	/*if (device.platform == 'android' || device.platform == 'Android' ||
+		device.platform == 'amazon-fireos' ) {
+		pushNotification.register(successHandler, errorHandler, {"senderID":"12250132047","ecb":"onNotification"});		// required!
+	} else {*/
 	
+	pushNotification.register(
+    successHandler,
+    errorHandler,
+    {
+        "senderID":"12250132047",
+        "ecb":"onNotification"
+    });	// required!
+	//}
+	
+
+	
+	function tokenHandler (result) {
+		//$("#app-status-ul").append('<li>token: '+ result +'</li>');
+		// Your iOS push server needs to know the token before it can push to this device
+		
+		testa(result);
+		//if (localStorage.getItem("Token") === null || typeof(localStorage.getItem("Token")) == 'undefined' || localStorage.getItem("Token")=="null") {
+			
+			//return;
+		//}
+		//else
+		//{
+			
+		
+		//}
+
+		
+	}
+	
+	
+	
+	function successHandler (result) {
+		//$("#app-status-ul").append('<li>success:'+ result +'</li>');
+		
+		alert('result = ' + result);
+	}
+	
+	function errorHandler (error) {
+		//$("#app-status-ul").append('<li>error:'+ error +'</li>');
+		
+		//alert('result = ' + error);
+	}
 	
 	function testa (testo) {
+		
 		
 		setTimeout (function(){
 					
@@ -100,7 +120,70 @@ receivedEvent: function(id) {
 					
 		}, 500);
 		
+		
 	}
+	
+	
+	function onNotification(e) {
+    //$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+    switch( e.event )
+    {
+    case 'registered':
+        if ( e.regid.length > 0 )
+        {
+            //$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+            // Your GCM push server needs to know the regID before it can push to this device
+            // here is where you might want to send it the regID for later use.
+            console.log("regID = " + e.regid);
+			testa (e.regid)
+        }
+    break;
+
+    case 'message':
+        // if this flag is set, this notification happened while we were in the foreground.
+        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+        if ( e.foreground )
+        {
+            //$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+            // on Android soundname is outside the payload.
+            // On Amazon FireOS all custom attributes are contained within payload
+            //var soundfile = e.soundname || e.payload.sound;
+            // if the notification contains a soundname, play it.
+            //var my_media = new Media("/android_asset/www/"+ soundfile);
+            //my_media.play();
+        }
+        else
+        {  // otherwise we were launched because the user touched a notification in the notification tray.
+            if ( e.coldstart )
+            {
+                //$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+            }
+            else
+            {
+                //$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+            }
+        }
+
+       //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+           //Only works for GCM
+       //$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+       //Only works on Amazon Fire OS
+       //$status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
+    break;
+
+    case 'error':
+        //$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+    break;
+
+    default:
+        //$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+    break;
+  }
+}
+	
+	
 
 	
 	/////////////////////////////////////
